53a74cf1e63f771e9e6c2192aafcf8639ffc053f
shakib2205 <shakibur35-2737@diu.edu.bd>
2023-09-06 12:57:32 +0600

    first commit

---
+++ nosto-bank/js/debug-techniqe.js
@@ -0,0 +1,12 @@
+/*
+debug step
+
+1. Error check (error reproduce)
+2.check others staff on the website
+3. check console for error
+4. click on the link of the error(it will take you to the code)
+5. if needed  add a breakpoint and try to stop the code over there and see the value.
+6. if needed console log output
+7. search full code base (ctrl + shift + F) > don't forget about partial match
+8. look around for typeof
+*/
